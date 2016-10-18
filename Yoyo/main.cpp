c13768e96a96a5365d2a944839fbb7ba9596cc9e
Samuel <samuel@samueldemacbook-air.local>
2016-10-17 19:27:16 -0700

    Initial Commit

---
+++ Yoyo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Yoyo
+//
+//  Created by Samuel on 10/17/16.
+//  Copyright © 2016 Samuel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
